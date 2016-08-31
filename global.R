bba6df381003145864d38b856fa292d00c681f6a
pelishk <pelishk@gmail.com>
2016-08-31 18:43:59 -0500

    modificación

--- global.R
+++ global.R
@@ -5,7 +5,7 @@ library(dplyr)
 datos<-readRDS('data/datos elektra,monte,first,prendamex.rds')
 
 datos<-datos %>%
-  filter(!is.na(latitude))%>%
-  filter(!is.na(longitude))
+  dplyr::filter(!is.na(latitude))%>%
+  dplyr::filter(!is.na(longitude))
 
 
